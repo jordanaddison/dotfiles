518261aabb5172588b029e82e6b161a7af4ce80e
Jordan Addison <jaddison@evidation.com>
2023-07-12 20:09:25 -0500

    Custom vim config

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,18 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "v2.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim"
+  },
+  config = function ()
+    require('neo-tree').setup() {}
+  end,
+}
